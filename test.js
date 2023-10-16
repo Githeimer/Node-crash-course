cfc966d5d1d48de3603354d64a22e6c3ba9c6ec8
Githeimer <dulalhimesh@gmail.com>
2023-10-16 20:45:34 +0545

    Day 1

--- test.js
+++ test.js
@@ -3,3 +3,35 @@ const name= 'himesh';
 console.log(name);
 console.log('I am learning node JS');
 
+// console.log(global);
+
+const os= require('os');
+
+// console.log(os);
+
+// console.log(os.type());
+// console.log(os.version());
+
+// console.log(__dirname)
+// console.log(__filename)
+
+// const path= require('path');
+
+// console.log(path.dirname(__filename));
+// console.log(path.basename(__filename));
+// console.log(path.extname(__filename));
+
+
+// console.log(path.parse(__filename))
+
+// const math= require('./math')
+
+const {add, subtract , multiply, divide}=require('./math')
+
+const value= divide(2,3);
+
+
+console.log(value);
+
+
+

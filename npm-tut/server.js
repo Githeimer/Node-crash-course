ff6138b7a38dee711fd88d1e734dd6aea66f7370
Himesh Dulal <108019916+Githeimer@users.noreply.github.com>
2023-11-05 20:29:25 +0545

    router handlers

--- npm-tut/server.js
+++ npm-tut/server.js
@@ -20,12 +20,33 @@ app.get('/old-page(.html)?', (req, res) => {
     res.redirect(301, './new-page.html');
 });
 
+//route handlers
 app.get('/hello(.html)?', (req, res, next) => {
     console.log('ATTEMPTED TO LOAD HELLO.html');
     next();
 }, (req, res) => {
-    res.redirect('../views/wao.html');
+    res.redirect('./wao.html');
 });
+
+//chaining route handlers
+
+const one = (req,res,next) => {
+    console.log("one");
+    next();
+}
+
+const two = (req,res,next)=>{
+    console.log("two");
+    next();
+}
+
+const three = (req,res) =>{
+    console.log("three");
+    res.send("FINISHED");
+}
+
+app.get('/chain(.html)?',[one,two,three]);
+
 
 app.get('/*', (req, res) => {
     res.status(404).sendFile(path.join(__dirname, '..', 'views', '404.html'));
